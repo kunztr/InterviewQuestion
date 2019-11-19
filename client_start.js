8c53803a4deb9c7dfa6f6eb2c8c6ebd8fed88b3f
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2019-11-19 14:36:42 -0700

    Adds basic Marionette applet

--- client_start.js
+++ client_start.js
@@ -1,1 +1,23 @@
+const {Application, View} = window.Marionette;
 
+const rootView = new (View.extend({
+
+    template() {
+        return "This is a test!";
+    }
+
+}));
+
+const app = new (Application.extend({
+
+    get region() {
+        return "body";
+    },
+
+    onStart() {
+        this.showView(rootView);
+    }
+
+}));
+
+app.start();
